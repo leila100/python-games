9a96b96665aab0f5c6c3623b7252314b6777d18f
Leila <nb.leila10@gmail.com>
2019-11-12 10:14:36 -0600

    Add paddles to the game

--- Pong/Pong1.py
+++ Pong/Pong1.py
@@ -6,7 +6,26 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)  # no automatic updates - makes game faster
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) # set speed to maximum possible speed
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1) # stretch the width to 5*20. Keep the len the same (20px) 
+paddle_a.penup()  # don't draw movements
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) # set speed to maximum possible speed
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1) # stretch the width to 5*20. Keep the len the same (20px) 
+paddle_b.penup()  # don't draw movements
+paddle_b.goto(350, 0)
+
+# Ball
+
 # Main game loop
 while True:
     wn.update() # update the screen for each loop run
-    
