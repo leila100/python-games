4e1031a3d211bee1883ea7745073ad381f611e3a
Leila <nb.leila10@gmail.com>
2019-11-12 11:07:43 -0600

    Add scoring message

--- Pong/Pong1.py
+++ Pong/Pong1.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)  # no automatic updates - makes game faster
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0) # set speed to maximum possible speed
@@ -33,6 +37,17 @@ ball.penup()  # don't draw movements
 ball.goto(0, 0)
 ball.dx = 0.25  
 ball.dy = -0.25 
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+# pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
+
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -79,9 +94,15 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
 
     # Paddle and ball collisions
     if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor()<paddle_b.ycor()+40 and ball.ycor()>paddle_b.ycor()-40 ):
